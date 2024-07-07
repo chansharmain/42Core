bd550fb0da6e12712738c70da5738fc8b9a1a8ce
chansharmain <chansharmain@gmail.com>
2024-07-08 02:17:54 +0800

    update

--- printf/ft_format_functions.c
+++ printf/ft_format_functions.c
@@ -6,31 +6,53 @@
 /*   By: shachan <shachan@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 01:53:10 by shachan           #+#    #+#             */
-/*   Updated: 2024/07/05 02:11:02 by shachan          ###   ########.fr       */
+/*   Updated: 2024/07/07 17:56:07 by shachan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "ft_printf.h"
 
 int	ft_putchar(char c)
 {
 	write(1, &c, 1);
+	return (1);
 }
 
 int	ft_putstr(char *str)
 {
 	int	i;
+	int	count;
 
 	i = 0;
+	count = 0;
 	while (str[i] != '\0')
 	{
 		write(1, &str[i], 1);
 		i++;
+		count++;
 	}
-}
-int	ft_putpointer(unsigned long long n)
-{
-	
+	return (count);
 }
 
+int	ft_putpointer(unsigned long long n)
+{
+	int	count;
+
+	count = 0;
+	if (n == 0)
+	{
+		write(1, "(nil)", 5);
+		count = 5;
+	}
+	else
+	{
+		write(1, "0x", 2);
+		count += 2;
+		count += ft_putnbr_base(n, "0123456789ABCDEF");
+	}
+	return (count);
+}
+/*
 int	ft_putnbr(long n)
 {
 	if (n < 0)
@@ -46,4 +68,45 @@ int	ft_putnbr(long n)
 	}
 	else
 		ft_putchar(n + '0');
+}
+*/
+
+int	ft_putunsigned(unsigned int n)
+{
+	int	count;
+
+	count = 0;
+	if (n < 10)
+	{
+		count = 1;
+		return (ft_putchar(n + '0'));
+	}
+	if (n > 9)
+	{
+		count += ft_putunsigned(n / 10);
+		count += ft_putchar(n % 10 + '0');
+	}
+	return (count);
+}
+
+int	ft_putnbr_base(long n, char *base)
+{
+	int	baselen;
+	int	count;
+
+	baselen = 0;
+	while (base[baselen] != '\0')
+		baselen++;
+	if (baselen < 1)
+		return (0);
+	count = 0;
+	if (n < 0)
+	{
+		count += ft_putchar('-');
+		n = -n;
+	}
+	if (n >= baselen)
+		count += ft_putnbr_base(n / baselen, base);
+	count += ft_putchar(base[n % baselen]);
+	return (count);
 }
--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,24 +6,24 @@
 /*   By: shachan <shachan@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 01:20:36 by shachan           #+#    #+#             */
-/*   Updated: 2024/07/06 02:09:43 by shachan          ###   ########.fr       */
+/*   Updated: 2024/07/07 18:16:44 by shachan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
-# define FT_PRINT_H
+# define FT_PRINTF_H
 
 # include <unistd.h>
-# include <stddef.h>
 # include <stdlib.h>
 # include <stdarg.h>
 
-static int	ft_format_specifier(char fs, va_list arg_ptr);
-int	ft_putchar(char c);
-int	ft_putstr(char *str);
-int	ft_putpointer(unsigned long long n);
-int	ft_putnbr(long n);
-
-int	ft_printf(const char *formatted_string, ...);
+//static int	ft_format_specifier(char fs, va_list arg_ptr);
+int			ft_putchar(char c);
+int			ft_putstr(char *str);
+int			ft_putpointer(unsigned long long n);
+//int	ft_putnbr(long n);
+int			ft_putunsigned(unsigned int n);
+int			ft_putnbr_base(long n, char *base);
+int			ft_printf(const char *formatted_string, ...);
 
 #endif
