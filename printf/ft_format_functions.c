bd8903525755c58a7601c5e78c1578f25229ee45
Sharmain Chan <shachan@c2r1s3.42singapore.sg>
2024-07-08 23:19:29 +0800

    test2

--- printf/ft_format_functions.c
+++ printf/ft_format_functions.c
@@ -6,17 +6,9 @@
 /*   By: shachan <shachan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 01:53:10 by shachan           #+#    #+#             */
-/*   Updated: 2024/07/08 22:36:32 by shachan          ###   ########.fr       */
+/*   Updated: 2024/07/08 22:50:18 by shachan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "ft_printf.h"
-
-#include "ft_printf.h"
-
-#include "ft_printf.h"
-
-#include "ft_printf.h"
 
 #include "ft_printf.h"
 
@@ -31,6 +23,8 @@ int	ft_putstr(char *str)
 	int	i;
 	int	count;
 
+	if (!str)
+		return(write(1, "(null)", 6));
 	i = 0;
 	count = 0;
 	while (str[i] != '\0')
@@ -56,28 +50,10 @@ int	ft_putpointer(unsigned long long n)
 	{
 		write(1, "0x", 2);
 		count += 2;
-		count += ft_putnbr_base(n, "0123456789ABCDEF");
+		count += ft_putnbr_base2(n, "0123456789abcdef");
 	}
 	return (count);
 }
-/*
-int	ft_putnbr(long n)
-{
-	if (n < 0)
-	{
-		write (1, "-", 1);
-		n = -n;
-		ft_putnbr(n);
-	}
-	else if (n > 9)
-	{
-		ft_putnbr(n / 10);
-		ft_putnbr(n % 10);
-	}
-	else
-		ft_putchar(n + '0');
-}
-*/
 
 int	ft_putunsigned(unsigned int n)
 {
@@ -97,10 +73,32 @@ int	ft_putunsigned(unsigned int n)
 	return (count);
 }
 
-int	ft_putnbr_base(long n, char *base)
+int	ft_putnbr_base2(unsigned long long n, char *base)
 {
-	int	baselen;
-	int	count;
+	unsigned long long	baselen;
+	unsigned long long	count;
+
+	baselen = 0;
+	while (base[baselen] != '\0')
+		baselen++;
+	if (baselen < 1)
+		return (0);
+	count = 0;
+	// if (n < 0)
+	// {
+	// 	count += ft_putchar('-');
+	// 	n = -n;
+	// }
+	if (n >= baselen)
+		count += ft_putnbr_base(n / baselen, base);
+	count += ft_putchar(base[n % baselen]);
+	return (count);
+}
+
+int	ft_putnbr_base(long long n, char *base)
+{
+	long long	baselen;
+	long long	count;
 
 	baselen = 0;
 	while (base[baselen] != '\0')
